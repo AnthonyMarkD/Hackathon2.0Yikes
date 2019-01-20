d7c83a5090ddb33f4c2ab493a382835b6c72f9b2
Osho Singh <asingh2@ualberta.ca>
2019-01-19 19:18:54 -0700

    Rigidbody sprites added

---
+++ hack2.0/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Movement script for player sprite. 
+ * Code was created with aid from Blackthornprod's youtube page
+ *
+ *  Video link https://www.youtube.com/watch?v=CeXAiaQOzmY
+ *  
+ */
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
+
