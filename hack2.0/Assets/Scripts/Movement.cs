09c19b649de26d8656659129617d05c9001eb130
Anthony Dmytryshyn <admytrys@ualberta.ca>
2019-01-19 19:51:44 -0700

    help

--- hack2.0/Assets/Scripts/Movement.cs
+++ hack2.0/Assets/Scripts/Movement.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+
+﻿/*
+ * Movement script for player sprite. 
+ * Code was created with aid from Blackthornprod's youtube page
+ *
+ *  Video link https://www.youtube.com/watch?v=CeXAiaQOzmY
+ *  
+ */
+
+
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
+
+
